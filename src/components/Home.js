87c4983397bd4da77d3057be01806d319cb5db80
mohittk <mvk1407@gmail.com>
2022-04-05 22:55:07 +0530

    tailwind config added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import logo from '../assets/logo.png'
 import circle from '../assets/circle.png'
-import './Home.css'
+import styles from '../styles/index.css';
+// import './Home.css';
 
 export default function Home(){
     return(
         <>
         <div className="home_page">
-            <div className="logo">
+            {/* <div className="logo">
                 <img src={logo} className="logo" />
             </div>
 
             <div className="circle">
                 <img src={circle} className="circle" />
-            </div>
+            </div> */}
 
             {/* <div className="circles">
                 <div className="c1"></div>
@@ -23,15 +24,15 @@ export default function Home(){
                
             </div> */}
             <div className="title">
-                <h1>Codame</h1>
-                <h3>Coding Club of IIIT Bhopal</h3>
+                <h1 className="text-blueGray-800 font-belleza text-6xl uppercase">Codame</h1>
+                <h3 className="text-black-300 font-inconsolota text-3xl ">Coding Club of IIIT Bhopal</h3>
             </div>
 
             <div className="waves">
             </div>
 
             <div className="hero_section">
-                <h1> hellloooo</h1>
+                
             </div>
             
 
