aad049372148e1268d2b7cced29b77886c3cb19f
gaurav147-star <gauravgupta9158@gmail.com>
2022-04-06 23:53:08 +0530

    fix:home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,25 +10,25 @@ export default function Home() {
     return (
         <>
 
-            <div className="home_page">
+            <div className="home_page h-screen">
                 <Navbar />
-                
-                <img src={circle} className="absolute top-0 right-0 w-130 z-1" />
 
-           
-                
-                <div className="hero_section m-10 p-8 w-full" >
+                <img src={circle} className="absolute top-0 right-0 w-[36rem] z-1" />
+
+
+
+                <div className="hero_section m-10 p-8" >
                     <img src={logo} className="float-left" />
                     <div className="title">
-                    <h1 className="text-darkBlue font-belleza text-9xl uppercase mt-20 z-5">Codame</h1>
-                    <h3 className="text-black font-inconsolota font-bold text-6xl mt-10 z-8">Coding Club of IIIT Bhopal</h3>
-                    
-                </div>
+                        <h1 className="text-darkBlue font-belleza text-9xl uppercase mt-20 z-5">Codame</h1>
+                        <h3 className="text-black font-inconsolota font-bold text-6xl mt-10 z-8">Coding Club of IIIT Bhopal</h3>
+
+                    </div>
 
                 </div>
-               
-                   
-              
+
+
+
 
 
 
