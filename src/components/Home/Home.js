b12ae1b7b9ea19aa4dc08175ab3552a223c44b7d
gaurav147-star <gauravgupta9158@gmail.com>
2022-04-06 10:51:51 +0530

    pages folder added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+
+        <div className='home_page'>
+            <img src="https://raw.githubusercontent.com/gaurav147-star/codame-website/main/public/assets/bg.png" alt="" className='w-full h-screen bg-cover bg-center' />
+        </div>
+    )
+}
+
+export default Home
