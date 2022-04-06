f6d1cfd017f03f755d912a6525a3329826558428
mohittk <mvk1407@gmail.com>
2022-04-06 11:38:54 +0530

    feat: updated folder structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles/index.css";
-import App from './App';
+import Router from './utils/Router';
+
 
 ReactDOM.render(
 
-    <App />,
+    <Router />,
 
   document.getElementById('root')
 );
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+import circle from '../assets/circle.png'
+import styles from '../styles/index.css';
+import Navbar from '../components/Navbar';
+
+
+export default function Home(){
+    return(
+        <>
+      
+        <div className="home_page">
+            <Navbar />
+            <div className="title">
+                <h1 className="text-darkBlue font-belleza text-6xl uppercase">Codame</h1>
+                <h3 className="text-black font-inconsolota text-3xl ">Coding Club of IIIT Bhopal</h3>
+            </div>
+
+    
+            
+
+
+
+        
+
+            
+        </div>
+      
+        </>
+    )
+}
