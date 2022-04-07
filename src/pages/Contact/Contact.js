6156f77e1992c8b979a1251de97c5402078ce37a
unknown <ujjgpta9@gmail.com>
2022-04-07 17:17:15 +0530

    made some changes

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,7 +10,7 @@ export default function Contact(){
         <Navbar active="contact" />
         <img src={event} alt="" className='absolute top-0 right-0' />
         <div className=''>
-            <h1 className='text-darkBlue font-belleza text-7xl uppercase mt-20 z-5 text-center'>Contact</h1>
+            <h1 className='text-darkBlue font-belleza text-9xl uppercase mt-20 z-5 text-center'>Contact</h1>
          
             <p className='text-black font-bold font-titanOne tracking-wide w-120 m-auto text-center mt-20 text-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque laborum nostrum magni cumque suscipit voluptates in, a, dolores harum, voluptate facilis odit fuga culpa enim quasi. Praesentium quibusdam provident sunt.</p>
       
