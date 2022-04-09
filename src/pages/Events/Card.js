18c1d6d43e700f75baaf9ee84a851b046217ef34
mitushi <mitushi2agrawal@gmail.com>
2022-04-09 15:25:47 +0530

    added card component for events

---
+++ src/pages/Events/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+      <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
+        <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
+      </div>
+    </>
+  )
+}
+
+export default Card
--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import triangle from '../../assets/triangle.svg'
 import '../../styles/index.css'
+import Card from './Card';
 
 
 // background: linear-gradient(169.82deg, rgba(3, 32, 71, 0) 35.33%, rgba(3, 32, 70, 0.19283) 59.04%, rgba(4, 31, 68, 0.73) 69.32%, #032047 91.7%);
@@ -24,30 +25,18 @@ export default function Events(){
         <div className="space-y-8">
         <div className="text-darkBlue text-center font-mono font-bold text-4xl">Upcoming events</div>
         <div className="flex flex-row flex justify-evenly">
-            <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
-                     <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
-            </div>
-            <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
-                     <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
-            </div>
-            <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
-                     <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
-            </div>
-            
+            <Card/>
+            <Card/>
+            <Card/>
         </div>
         </div>
         <div className="space-y-8">
         <div className="text-darkBlue text-center font-mono font-bold text-4xl">Past events</div>
         <div className="flex flex-row flex justify-evenly">
-            <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
-                     <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
-            </div>
-            <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
-                     <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
-            </div>
-            <div className="relative h-44 w-60 bg-emerald-300 rounded-3xl ">
-                     <div className="absolute h-44 w-60 -left-4 -top-4 bg-emerald-200 rounded-3xl -z-20 opacity-80"></div>
-            </div>
+        <Card/>
+        <Card/>
+        <Card/>
+            
        
         </div>
         </div>
