0d56252da8aaa2339043f82000d65094e70a7c6e
mitushi <mitushi2agrawal@gmail.com>
2022-04-09 20:34:04 +0530

    added bg in event page

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
+import React from 'react'
+import Navbar from '../../components/Navbar'
 import triangle from '../../assets/triangle.svg'
 import '../../styles/index.css'
-import Card from './Card';
+import Card from './Card'
 
 
-// background: linear-gradient(169.82deg, rgba(3, 32, 71, 0) 35.33%, rgba(3, 32, 70, 0.19283) 59.04%, rgba(4, 31, 68, 0.73) 69.32%, #032047 91.7%);
-//         filter: blur(144px);
-//         transform: rotate(15deg);
-
-export default function Events(){
-    return(
-        <div className=''>
-
-        <Navbar active="events"/>
-        <img src={triangle} alt="" className='absolute top-0 right-0' />
+export default function Events() {
+  return (
+    <>
+      <div className="">
+        <Navbar active="events" />
+        <img src={triangle} alt="" className="absolute top-0 right-0" />
         <div className="space-y-44">
-        <div className=''>
-            <h1 className='text-darkBlue font-encodeSans text-9xl uppercase mt-20 z-5 text-left ml-32'> Events</h1>
-         
-            <p className='text-black font-bold font-titanOne tracking-wide w-120 m-auto text-center mt-20 text-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque laborum nostrum magni cumque suscipit voluptates in, a, dolores harum, voluptate facilis odit fuga culpa enim quasi. Praesentium quibusdam provident sunt.</p>
-      
-        </div>
-        <div className="space-y-24">
-        <div className="text-darkBlue text-center font-mono font-bold text-4xl">Upcoming events</div>
-        <div className="flex flex-row flex justify-evenly">
-            <Card/>
-            <Card/>
-            <Card/>
-        </div>
-        </div>
-        <div className="space-y-24">
-        <div className="text-darkBlue text-center font-mono font-bold text-4xl">Past events</div>
-        <div className="flex flex-row flex justify-evenly">
-        <Card/>
-        <Card/>
-        <Card/>
-            
-       
-        </div>
-        </div>
-       
-        </div>
-        </div>
+          <div className="">
+            <h1 className="text-darkBlue font-encodeSans text-9xl uppercase mt-20 z-5 text-left ml-32">
+              {' '}
+              Events
+            </h1>
 
-    )
-
+            <p className="text-black font-bold font-titanOne tracking-wide w-120 m-auto text-center mt-20 text-2xl">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+              laborum nostrum magni cumque suscipit voluptates in, a, dolores
+              harum, voluptate facilis odit fuga culpa enim quasi. Praesentium
+              quibusdam provident sunt.
+            </p>
+          </div>
+          <div className="space-y-24">
+            <div className="text-darkBlue text-center font-mono font-bold text-4xl">
+              Upcoming events
+            </div>
+            <div className="flex flex-row justify-evenly">
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+          <div className="space-y-24">
+            <div className="text-darkBlue text-center font-mono font-bold text-4xl">
+              Past events
+            </div>
+            <div className="flex flex-row flex justify-evenly">
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+        </div>
+        <div className="bg"></div>
+      </div>
+    </>
+  )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,48 +1,33 @@
-import React from 'react';
-import styles from '../../styles/index.css';
-import Navbar from '../../components/Navbar';
-import circle from '../../assets/circle.svg';
-import logo from '../../assets/logo.png';
-import vector from '../../assets/vector.svg';
-
-
+import React from 'react'
+import styles from '../../styles/index.css'
+import Navbar from '../../components/Navbar'
+import circle from '../../assets/circle.svg'
+import logo from '../../assets/logo.png'
+import vector from '../../assets/vector.svg'
 
 export default function Home() {
-    return (
+  return (
+    <div className="home_page">
+      <Navbar active="home" />
 
+      <img
+        src={circle}
+        className=" w-[14rem] absolute top-0 right-0 z-1 sm:w-[14rem] md:w-[25rem] lg:w-[30rem]"
+      />
 
-        <div className="home_page">
-            <Navbar active="home" />
-
-            <img src={circle} className=" w-[14rem] absolute top-0 right-0 z-1 sm:w-[14rem] md:w-[25rem] lg:w-[30rem]" />
-
-            <div className="hero_section m-10 p-8" >
-                <img src={logo} className="float-left h-50 w-[32rem] lg:h-70 " />
-                <div className="title">
-                    <h1 className="text-darkBlue font-belleza uppercase mt-20 z-5 text-5xl text-center sm:text-5xl sm:text-left md:text-7xl md:text-left lg:text-9xl lg:text-left">Codame</h1>
-                    <h3 className="text-black font-inconsolota font-bold mt-10 z-8 text-3xl text-center sm:text-3xl sm:text-left md:text-5xl md:text-left lg:text-7xl lg:text-left">Coding Club of IIIT Bhopal</h3>
-
-                </div>
-
-            </div>
-
-
-            <div className="buuu">
-
-            </div>
-
-
-
-
-
-
-
-
-
-
-
+      <div className="hero_section m-10 p-8">
+        <img src={logo} className="float-left h-50 w-[32rem] lg:h-70 " />
+        <div className="title">
+          <h1 className="text-darkBlue font-belleza uppercase mt-20 z-5 text-5xl text-center sm:text-5xl sm:text-left md:text-7xl md:text-left lg:text-9xl lg:text-left">
+            Codame
+          </h1>
+          <h3 className="text-black font-inconsolota font-bold mt-10 z-8 text-3xl text-center sm:text-3xl sm:text-left md:text-5xl md:text-left lg:text-7xl lg:text-left">
+            Coding Club of IIIT Bhopal
+          </h3>
         </div>
+      </div>
 
-
-    )
+      <div className="buuu"></div>
+    </div>
+  )
 }
