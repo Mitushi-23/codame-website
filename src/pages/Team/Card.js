74ebd04270785e6590dd77a57a595593a67156f1
yashita-jain <jainyashita06@gmail.com>
2022-04-10 16:03:27 +0530

    Team Page: Icons on hover

--- src/pages/Team/Card.js
+++ src/pages/Team/Card.js
@@ -7,30 +7,29 @@ import githubImg from "../../assets/github.svg";
 import twitterImg from "../../assets/twitter.svg";
 import instagramImg from "../../assets/instagram.svg";
 
+
 export default function Card(props) {
 
     const [group, setGroup] = useState();
     
-
-
-  return (
+return (
     <div className="card-container">
-      <div class="container grid grid-cols-3 gap-3 mx-auto">
-        <div class="w-40">
-          <img className="rounded-full border-8 border-lightGreen" src={team} />
-          <h3 className="uppercase mt-3 text-center text-darkBlue font-semibold">
-            {props.name}
-          </h3>
-          <div className="w-40 hidden group-hover:block">
-          <div className="grid grid-cols-4 gap-4">
-            <img className="linkedin" src={linkedinImg} />
-            <img className="github" src={githubImg} />
-            <img className="twitter" src={twitterImg} />
-            <img className="instagram" src={instagramImg} />
-            </div>
+    <div class="container grid grid-cols-3 gap-3 mx-auto group ">
+      <div class="w-40">
+        <img className="rounded-full border-8 border-lightGreen" src={team} />
+        <h3 className="uppercase mt-3 text-center text-darkBlue font-semibold">
+          {props.name}
+        </h3>
+        <div className="w-40 hidden group-hover:block absolute">
+        <div className="grid grid-cols-4 gap-4">
+       <a href="https://reactjs.org/">  <img className="linkedin" src={linkedinImg} /></a> 
+       <a href="https://reactjs.org/">     <img className="github" src={githubImg} /></a>
+       <a href="https://reactjs.org/">     <img className="twitter" src={twitterImg} /></a>
+       <a href="https://reactjs.org/">   <img className="instagram" src={instagramImg} /></a>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
